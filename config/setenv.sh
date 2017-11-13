66be37f486c7632e39d5239e559ce09fe8ca1c31
yrong <yrong1997@gmail.com>
2017-11-13 23:04:21 +0800

    增加license目录&删除neo4j废弃配置&变更为NODE_CONFIG_DIR(0.5h)

--- config/setenv.sh
+++ config/setenv.sh
@@ -2,5 +2,6 @@
 
 export LOG_PATH=/ssd/logs
 export RUNTIME_PATH=/ssd/runtime
-export CONFIG_PATH=/ssd/mynode/config/config
+export NODE_CONFIG_DIR=/ssd/mynode/config/config
+export LICENSE_PATH=/ssd/license
 export NEO4J_HOME=/ssd/servers/neo4j-community-3.1.1
