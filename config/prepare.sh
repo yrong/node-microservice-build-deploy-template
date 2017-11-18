3f26bac60b713ee9bb775cfc03346826bbc42b15
yrong <yrong1997@gmail.com>
2017-11-18 11:33:22 +0800

    docker容器化封装改造(6h)

---
+++ config/prepare.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+dbs=("elasticsearch" "redis" "neo4j" "pg-$PG_DB_KB" "pg-$PG_DB_NOTIFICATION")
+
+for db in ${dbs[@]}
+do
+    echo $db
+    mkdir -p $DB_DATA/$db
+    sudo chmod -R 0777 $DB_DATA/$db
+done
--- config/setenv.sh
+++ config/setenv.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 
+# 程序日志目录
 export LOG_PATH=/ssd/logs
+
+# 程序运行期数据目录
 export RUNTIME_PATH=/ssd/runtime
-export NODE_CONFIG_DIR=/ssd/mynode/config/config
+
+# license目录
 export LICENSE_PATH=/ssd/license
-export NEO4J_HOME=/ssd/servers/neo4j-community-3.1.1
+
+# 数据库数据目录
+export DB_DATA=/ssd/data
+
+# 程序配置目录
+export NODE_CONFIG_DIR=/ssd/mynode/config/config
+
+# 本机非loopback地址
+export PRIVATE_IP=192.168.0.100
+
+# neo4j
+export NEO4J_HOST=$PRIVATE_IP
+export NEO4J_USER=neo4j
+export NEO4J_PASSWD=admin
+
+# redis
+export REDIS_HOST=$PRIVATE_IP
+
+# elasticsearch
+export ES_HOST=$PRIVATE_IP
+
+# postgresql
+export PG_HOST=$PRIVATE_IP
+export PG_USER=postgres
+export PG_PASSWD=postgres
+export PG_DB_KB=kb
+export PG_DB_NOTIFICATION=notification
+export PG_ZHPARSER=chinese
